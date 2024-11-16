9959d46cf06c614fad93e3a1324742c287e768c4
Romain <contact@romaindu35.fr>
2024-11-16 19:24:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# NgPortUnlocker
+
+<img alt="Un magnifique logo fait par ChatGPT" src="ngportunlocker.png" title="NgPortUnlocker" width="100"/>
+
+Ce projet permet de "demander" au pare-feu de NationsGlory de débloquer l'accès aux serveurs java.
+En effet, depuis le 13 novembre 2024, les mesures de sécurité de NationsGlory ont été renforcées et il n'est désormais
+plus possible de communiquer directement avec les serveurs java. Cela a été réalisé afin de limiter les attaques DDoS.
+
+> ⚠️ **Attention :** Il vous revient de sécuriser l'accès à l'api exposée. Si celle-ci est utilisé à des fins malveillantes, vous pourriez avoir quelques problèmes...
+
+## Comment ça marche ?
+
+Ce projet émule le fonctionnement du client NationsGlory afin d'obtenir les accès.
+POur cela, il se connecte au socket qui permet d'ouvrir les ports.
+Il est donc nécessaire de s'authentifier avec un compte valide.
+
+## Configuration
+
+Afin de faire fonctionner le projet, il est obligatoire de renseigner les informations de connexion.
+Pour cela, créer un fichier `config.toml` au même niveau que l'exécutable.
+
+```toml
+auth_string = "<token>" # Votre token d'authentification
+address = "127.0.0.1" # L'adresse sur laquelle doit être ouvert le serveur
+port = 8080 # Le port sur lequel doit être ouvert le serveur
+```
+
+Concernant le token d'authentification, il est composé de 3 parties :
+`<pseudo>##<session_token>##<hash>`
+- `<pseudo>` : Votre pseudo
+- `<session_token>` : Récupérable depuis le fichier `%AppData%/NationsGlory/config.json`
+- `<hash>` : Il est calculé automatiquement par le jeu sur la base de différents paramètres. Mais doit être le même que celui utilisé lors de la connexion.
+
+Au vu de la difficulté de récupération du hash, je conseille personnellement d'utiliser un intercepteur de packet comme wireshark afin de l'obtenir. \
+Pour cela, lancer votre intercepteur au lancement du jeu et chercher le bon packet.
+Pour vous aider, vous pouvez utiliser le filtre suivant et le packet sera parmi les premiers : 
+```bash
+ip.dst == 185.152.25.81 && tcp.port == 59001
+```
+
+## Utilisation
+Une fois le serveur lancé, afin de débloquer le port du serveur **pour l'ip courante**, il suffit de faire une requête GET sur le serveur.
+
+```bash
+curl http://<address>:<port>/connect/<server>
+```
+
+A l'heure actuelle, voici les serveurs recensés : "blue", "orange", "yellow", "white", "black", "cyan", "lime",
+"coral", "pink", "purple", "green", "red", "accueil", "hub",
+"freebuild1", "freebuild2", "freebuild3"
+
+Voici les ports associés (L'ip étant toujours, à l'heure actuelle : **185.152.25.81**) :
+- blue: 26012
+- orange: 26008
+- yellow: 26011
+- white: 26009
+- black: 26000
+- cyan: 26004
+- lime: 26005
+- coral: 26002
+- pink: 26001
+- purple: 26003
+- green: 26006
+- red: 26007
+- mocha: 26016
+- tutoriel (accueil): 12333
+- hub: 12555
+- island 1: 17123
+- island 2: 17124
+- island 3: 17126
---
+++ src/main.rs
@@ -0,0 +1,116 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::serde::json::Json;
+use rocket::State;
+use std::io::{self, BufRead, BufReader, Write};
+use std::net::TcpStream;
+use std::sync::mpsc::Sender;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+use config::Config;
+use serde::Deserialize;
+
+const SERVER_ADDRESS: &str = "event.nationsglory.fr:59001";
+const REMOVE_WAITLIST: &str = "MESSAGE socket REMOVE_WAITINGLIST";
+
+#[derive(Debug, Deserialize)]
+struct AppConfig {
+    auth_string: String,
+    address: String,
+    port: u16,
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let (tx, rx) = std::sync::mpsc::channel::<String>();
+
+    let settings = Config::builder()
+        .set_default("port", 8000).unwrap()
+        .set_default("address", "127.0.0.1").unwrap()
+        .add_source(config::File::with_name("config"))
+        .build()
+        .unwrap();
+
+    let config: AppConfig = settings.try_deserialize().unwrap();
+
+    if config.auth_string.is_empty() {
+        eprintln!("Le champ auth_string est vide dans le fichier de configuration. Merci de le spécifier !");
+        return Ok(());
+    }
+
+    let auth_string = config.auth_string.clone();
+    thread::spawn(move || listen_to_server(rx, auth_string));
+
+    rocket::build()
+        .manage(tx)
+        .mount("/", routes![connect])
+        .configure(rocket::Config::figment().merge(("port", &config.port)).merge(("address", &config.address)))
+        .launch()
+        .await?;
+
+    Ok(())
+}
+
+#[get("/connect/<server>")]
+async fn connect(server: &str, tx: &State<Sender<String>>) -> String {
+    if server.is_empty() {
+        return "Le serveur ne peut-être vide".to_string();
+    }
+
+    tx.send(server.to_string()).unwrap();
+    format!("Message envoyé : {}", server)
+}
+
+fn listen_to_server(rx: std::sync::mpsc::Receiver<String>, auth_string: String) {
+    let stream = TcpStream::connect(SERVER_ADDRESS);
+    let mut stream = match stream {
+        Ok(s) => {
+            println!("Connecté au serveur");
+            Some(s)
+        }
+        Err(e) => {
+            eprintln!("Erreur de connexion au serveur: {}", e);
+            None
+        }
+    };
+
+    let mut is_auth = false;
+
+    if let Some(mut tcp_stream) = stream {
+        let reader = BufReader::new(tcp_stream.try_clone().expect("Erreur de clonage du flux"));
+
+        for line in reader.lines() {
+            match line {
+                Ok(server_message) => {
+                    if !server_message.starts_with("PING_AND_DATA") {
+                        println!("Message reçu du serveur : {}", server_message);
+
+                        if server_message.starts_with("SUBMITNAME") {
+                            writeln!(tcp_stream, "{}", auth_string).unwrap();
+                        }
+
+                        if server_message.starts_with("NAMEACCEPTED") {
+                            is_auth = true;
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Erreur lors de la lecture du message : {}", e);
+                    break;
+                }
+            }
+
+            if let Ok(server) = rx.try_recv() {
+                if is_auth {
+                    let message = format!("MESSAGE socket ADD_WAITINGLIST {}", server);
+                    writeln!(tcp_stream, "{}", message).unwrap();
+                    writeln!(tcp_stream, "{}", REMOVE_WAITLIST).unwrap();
+                }
+            }
+        }
+    }
+
+    println!("Connexion fermée par le serveur.");
+}
